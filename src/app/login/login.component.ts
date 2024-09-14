1e380cbcacd370b51b5a733d92ea3c3851f028a8
Emiliano <eadecuzzi@estudiantes.unsam.edu.ar>
2024-09-14 20:46:55 -0300

    Cambios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'readapp-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
