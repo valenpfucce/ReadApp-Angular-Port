93385a81e7c0780cf76afdf9b5286f439f3418e4
Emiliano <eadecuzzi@estudiantes.unsam.edu.ar>
2024-09-14 15:32:48 -0300

    404 Not Found agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component'
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component:LoginComponent },
-    { path: '**', component:LoginComponent }
+    { path: '**', component:NotfoundComponent}
 ];
