b5888bda58d70802cc894b8484e33edc3edc5992
Emiliano <eadecuzzi@estudiantes.unsam.edu.ar>
2024-09-14 21:25:44 -0300

    Pagina ejemplo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { NotfoundComponent } from './notfound/notfound.component';
+import { PaginaEjemploComponent } from './pagina-ejemplo/pagina-ejemplo.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'ejemplo', component:PaginaEjemploComponent },
     { path: 'login', component:LoginComponent },
     { path: '**', component:NotfoundComponent}
 ];
