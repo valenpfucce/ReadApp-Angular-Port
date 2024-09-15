95c4a8c233a06ebf2537690871ca00aad5769777
agusreyb <a.reybrienza@gmail.com>
2024-09-15 20:31:33 -0300

    creo la card valoracion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './login/login.component'
 import { NotfoundComponent } from './notfound/notfound.component';
 import { PaginaEjemploComponent } from './pagina-ejemplo/pagina-ejemplo.component';
 import { BusquedaRecomendacionesComponent } from './busqueda-recomendaciones/busqueda-recomendaciones.component';
+import { CardValoracionComponent } from './card-valoracion/card-valoracion.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'ejemplo', component:PaginaEjemploComponent },
     { path: 'login', component:LoginComponent },
     { path: 'home', component:BusquedaRecomendacionesComponent},
+    { path: 'detalle_recomendacion', component:CardValoracionComponent},
     { path: '**', component:NotfoundComponent}
 ];
---
+++ src/app/card-valoracion/card-valoracion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'readapp-card-valoracion',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './card-valoracion.component.html',
+  styleUrl: './card-valoracion.component.css'
+})
+export class CardValoracionComponent {
+
+}
