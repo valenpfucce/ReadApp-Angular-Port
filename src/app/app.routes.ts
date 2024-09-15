b21b593180670887f76ac599858f0eb9e58d8700
TatiSabba <tatisabba@gmail.com>
2024-09-15 15:19:34 -0300

    Creo componente card recomendacion y pagina de busqueda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { NotfoundComponent } from './notfound/notfound.component';
 import { PaginaEjemploComponent } from './pagina-ejemplo/pagina-ejemplo.component';
+import { BusquedaRecomendacionesComponent } from './busqueda-recomendaciones/busqueda-recomendaciones.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'ejemplo', component:PaginaEjemploComponent },
     { path: 'login', component:LoginComponent },
+    { path: 'home', component:BusquedaRecomendacionesComponent},
     { path: '**', component:NotfoundComponent}
 ];
