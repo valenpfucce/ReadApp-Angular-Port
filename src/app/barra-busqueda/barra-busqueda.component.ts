e080c1ad50a2d6c55390f3b308454eaff37e595a
emiferretti <emieferretti@gmail.com>
2024-09-17 12:47:20 -0300

    upload barra_busqueda component

---
+++ src/app/barra-busqueda/barra-busqueda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-barra-busqueda',
+  standalone: true,
+  imports: [],
+  templateUrl: './barra-busqueda.component.html',
+  styleUrl: './barra-busqueda.component.css'
+})
+export class BarraBusquedaComponent {
+
+}
