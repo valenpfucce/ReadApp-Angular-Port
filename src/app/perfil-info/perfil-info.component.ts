8366dc261be5f5ef4e481bf72a2cfa6ba9846680
valenpfucce <valenfucce@gmail.com>
2024-09-18 00:12:30 -0300

    validacionnes mas completas, NO ELIMINAR RAMA PLS

--- src/app/perfil-info/perfil-info.component.ts
+++ src/app/perfil-info/perfil-info.component.ts
@@ -1,29 +1,34 @@
 import { Component } from '@angular/core';
-// import dayjs from 'dayjs'
+import dayjs from 'dayjs'
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarPerfilComponent } from '../sidebar-perfil/sidebar-perfil.component';
 import { Usuario } from '../usuario/usuario';
+import { ValidacionFieldComponent } from "./validacion-field/validacion-field.component";
 
 @Component({
   selector: 'readapp-perfil-info',
   standalone: true,
-  imports: [HeaderComponent, SidebarPerfilComponent, FormsModule],
+  imports: [HeaderComponent, SidebarPerfilComponent, FormsModule, ValidacionFieldComponent],
   templateUrl: './perfil-info.component.html',
   styleUrl: './perfil-info.component.css'
 })
 
 export class PerfilInfoComponent {
   usuario = new Usuario()
+  fechaNacimiento= ''
 
 
  guardar() {
+  this.usuario.fechaNacimiento = this.fechaNacimiento === '' ? undefined : dayjs(this.fechaNacimiento).toDate()
   this.usuario.guardarDatos()
+  
  }
  
  
 
- cancelar() {/* logica para poner los campos en blanco y quiza antes otra confirmacion*/}
+ cancelar() {
+  /* logica para poner los campos en blanco y quiza antes otra confirmacion*/}
 
 
 }
