fb2ef283dd37870e1593979d435d2da0861213dc
valenpfucce <valenfucce@gmail.com>
2024-09-28 14:09:27 -0300

    sidebar con for

--- src/app/sidebar-perfil/sidebar-perfil.component.ts
+++ src/app/sidebar-perfil/sidebar-perfil.component.ts
@@ -1,17 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'readapp-sidebar-perfil',
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule,CommonModule],
   templateUrl: './sidebar-perfil.component.html',
   styleUrl: './sidebar-perfil.component.css'
 })
 export class SidebarPerfilComponent {
 
+  infoSidebar = [
+   
+    new menuSidebar(
+      "Informacion",
+      '/imagenes/info.svg',
+      'info'
+    ),
+  
+    new menuSidebar(
+      "Amigos",
+      "/imagenes/amigos.svg" ,
+      'amigos'
+    ),
+    
+    new menuSidebar(
+      "Libos Leidos",
+      "/imagenes/libros.svg",
+      'info'
+    ),
+  
+    new menuSidebar(
+      "Libros a leer",
+      "/imagenes/librosaleer.svg",
+      'info'
+    ),
+  
+    new menuSidebar(
+      "Recomendaciones a valorar",
+      "/imagenes/valorarReco.svg",
+      'recomendacionesAValorar'
+    ),
+  
+  ]
+  
+}
 
+export class menuSidebar{
+
+  constructor(
+    public nombre: string,
+    public icono_url: string,
+    public router: string,
+   
+  ){}
 }
 
