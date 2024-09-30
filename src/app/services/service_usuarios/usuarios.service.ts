0d28591c04ebcbab7a0b088a43aa41cda4c89f15
agusreyb <a.reybrienza@gmail.com>
2024-09-30 15:10:35 -0300

    arreglo validaciones y algunos comportamientos

--- src/app/services/service_usuarios/usuarios.service.ts
+++ src/app/services/service_usuarios/usuarios.service.ts
@@ -1,26 +1,27 @@
-import { Injectable } from '@angular/core';
-import { usuarios } from '../../mocks/mock_usuarios';
-import { sesionesUsuarios } from '../../mocks/mock_usuariossesiones';
+import { Injectable } from '@angular/core'
+import { usuarios } from '../../mocks/mock_usuarios'
+import { sesionesUsuarios } from '../../mocks/mock_usuariossesiones'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
-  private readonly sessionKey = 'userSession';
-  constructor() { }
+  private readonly sessionKey = 'userSession'
+  constructor() {}
 
-  login(usuario: string, contrasenia: string) : Boolean{
-    if (usuario === 'admin' && contrasenia === 'password') {
-      sessionStorage.setItem('user', JSON.stringify({ usuario }));
-      console.log("TODO OK USUARIO")
-      return true;
+  login(mail: string, contrasenia: string): Boolean {
+    const usuarioEncontrado = sesionesUsuarios.find(
+      (usuario) =>
+        usuario.mail.trim().toLowerCase() === mail.trim().toLowerCase() &&
+        usuario.pass.trim() === contrasenia.trim()
+    )
+
+    if (usuarioEncontrado) {
+      sessionStorage.setItem(this.sessionKey, JSON.stringify(usuarioEncontrado))
+      console.log('Login exitoso: ', usuarioEncontrado)
+      return true
     }
-    console.log("TODO MAL")
-    return true;
+    console.log('Usuario o contraseña incorrectos')
+    return false
   }
-  
-
-  //this.libros.find(libro => libro.id === id)
-} 
-
-
+}
