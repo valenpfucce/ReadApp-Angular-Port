af594fcff5b1650338f80cad0cf693f972c8ae26
EmilianoD <eadecuzzi@estudiantes.unsam.edu.ar>
2024-10-10 13:48:49 -0300

    Cambios (no andan)

--- src/app/domain/recomendacion.ts
+++ src/app/domain/recomendacion.ts
@@ -2,6 +2,17 @@ import { Entidad } from "./entidad";
 import { Libro } from "./libro";
 import { Valoracion } from "./valoracion";
 
+
+export type RecomendacionJSON = {
+  id: number,
+  creadorId: number, //NO DEBERIA TRAER EL CREADOR ID, CHEQUEAR EN EL BACK
+  titulo: string,
+  publica: boolean,
+  descripcion: string,
+  lista_libros: string[],
+  valoraciones: string[]
+}
+
 export class Recomendacion implements Entidad{
     constructor(
       public id: number,
@@ -11,13 +22,28 @@ export class Recomendacion implements Entidad{
       public descripcion: string,
       public lista_libros: Libro[],
       public valoraciones: Valoracion[],
-      public tiempoLectura: string
+      public tiempoLectura?: string
     ){}
 
     promedioValoraciones(){
-        return this.valoraciones.length === 0 
-            ? 0 
+        return this.valoraciones.length === 0
+            ? 0
             : (this.valoraciones.map(valoracion => valoracion.valoracion).reduce((a, b) => a + b, 0) / this.valoraciones.length).toFixed(1);
     }
 
+  static fromJson(recomendacionJSON: RecomendacionJSON): Recomendacion {
+    // @ts-ignore
+    return Object.assign(new Recomendacion(), recomendacionJSON, {
+      lista_libros: recomendacionJSON.lista_libros
+        ?
+      // asignatario: tareaJSON.asignadoA
+      //   ? Usuario.fromJSON(tareaJSON.asignadoA)
+      //   : undefined,
+      // fecha: tareaJSON.fecha
+      //   ? DateTime.fromFormat(tareaJSON.fecha, FORMATO_FECHA).toJSDate()
+      //   : undefined
+    })
+  }
+
 }
+
--- src/app/services/service_recomendaciones/recomendaciones.service.ts
+++ src/app/services/service_recomendaciones/recomendaciones.service.ts
@@ -1,20 +1,48 @@
 import { Injectable } from '@angular/core';
 import { recomendaciones } from '../../mocks/mock_recomendaciones';
-import { Usuario } from '../../domain/usuario';
+import {REST_SERVER_URL} from "../configuration";
+import { HttpClient } from '@angular/common/http'
+import {catchError, map} from "rxjs/operators";
+import {lastValueFrom, of} from "rxjs";
+import {UsuarioLoginJSON} from "../service_usuarios/usuarios.service";
+import {Recomendacion} from "../../domain/recomendacion";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecomendacionesService {
   listaRecomendaciones = recomendaciones
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   listar_recomendaciones(){
     return this.listaRecomendaciones
   }
 
+  // async getRecomendacion(id: number) : Recomendacion {
+  //   const recomendacionEncontrada = this.httpClient
+  //     .get<UsuarioLoginJSON>(`${REST_SERVER_URL}/recomendaciones/${id}`)
+  //     .pipe(
+  //       map((response) => response?.id || null),
+  //       catchError((error) => {
+  //         return of(null) // Retornar null en caso de error
+  //       })
+  //     )
+  // }
+
   getRecomendacion(id: number) {
     return this.listaRecomendaciones.find(recomendacion => recomendacion.id === id)
+  }
+
+  async getRecomendacionCompleta(id: number): Promise<Recomendacion | null> {
+    try {
+      const recomendacionJSON$ =
+        this.httpClient.get<Recomendacion>(`${REST_SERVER_URL}/recomendaciones/${id}`)
+      const recomendacionJSON = await lastValueFrom(recomendacionJSON$)
+      return recomendacionJSON ? Recomendacion.fromJson(recomendacionJSON) : undefined
+    } catch (error) {
+      console.error('Error al obtener recomendación:', error);
+      return null; // Retorna null en caso de error
+    }
   }
 
   busquedaGeneral(palabraABuscar?: string){
