d787851cf32103c8388ec352672e435aa6f68a40
Emiliano <eadecuzzi@estudiantes.unsam.edu.ar>
2024-10-13 15:24:20 -0300

    Info Perfil Arreglado?

--- src/app/services/service_user_session_storage/user-session-storage.service.ts
+++ src/app/services/service_user_session_storage/user-session-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../service_usuarios/usuarios.service';
 import { firstValueFrom } from 'rxjs';
+import {Usuario} from "../../domain/usuario";
 
 
 @Injectable({
@@ -12,13 +13,11 @@ export class UserSessionStorageService {
 
   errors: String[] = [];
   private readonly sessionKey = 'userSession';
-  
+
   constructor(
     private router : Router,
-    private userService: UsuariosService,
+    private userService: UsuariosService
   ) { }
-
-  
   // ===== SESSION STORAGE ===== //
 
 
@@ -31,25 +30,43 @@ export class UserSessionStorageService {
       // Usamos firstValueFrom para convertir el observable en una promesa
       const response = await firstValueFrom(this.userService.putVerificationUser(mail, contrasenia))
       sessionStorage.setItem(this.sessionKey, response!.toString())
+      const usuarioEncontrado: Usuario = await this.obtenerDatosUsuario(response)
+      sessionStorage.setItem("user_name", usuarioEncontrado.nombre)
+      sessionStorage.setItem("user_lastname", usuarioEncontrado.apellido)
+      sessionStorage.setItem("user_img", usuarioEncontrado.img_perfil)
       return response
     } catch (error) {
       console.error('Error en el login:', error)
       throw error
     }
   }
-  
+
   obtenerIDuserSS(): number | null {
     const idUser = sessionStorage.getItem(this.sessionKey)
     return idUser ? parseInt(idUser,10): null
   }
 
-  
+  obtenerNombreUserSS(){
+    return sessionStorage.getItem("user_name")
+  }
+  obtenerApellidoUserSS(){
+    return sessionStorage.getItem("user_lastname")
+  }
+  obtenerImgUserSS(){
+    return sessionStorage.getItem("user_img")
+  }
+
+  async obtenerDatosUsuario(userIdSS : number | null ){
+    const usuarioEnLinea = await this.userService.getUserId(userIdSS)
+    return usuarioEnLinea;
+  }
+
   addError(mensajeError: string) {
       this.errors.push(mensajeError);
   }
- 
+
 
   navegarAHome(){this.router.navigate(['/login']);}
 }
-  
+
 
