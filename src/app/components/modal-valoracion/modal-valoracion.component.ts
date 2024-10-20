53fda71e0c8419103111eca6b31f85e47d471b93
Emiliano <eadecuzzi@estudiantes.unsam.edu.ar>
2024-10-19 21:09:46 -0300

    Valoracion Modal hecha en Recomendacion

--- src/app/components/modal-valoracion/modal-valoracion.component.ts
+++ src/app/components/modal-valoracion/modal-valoracion.component.ts
@@ -3,22 +3,29 @@ import { CommonModule } from '@angular/common'
 import { Usuario } from '../../domain/usuario'
 import { UsuariosService } from '../../services/service_usuarios/usuarios.service'
 import { UserSessionStorageService } from '../../services/service_user_session_storage/user-session-storage.service'
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'readapp-moda-valoracion',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './modal-valoracion.component.html',
   styleUrls: [
     '../../estilos_generales/cartas_libros.css',
+    '../../estilos_generales/estilo_recomendacion.css',
     './modal-valoracion.component.css'
   ]
 })
 export class ModalValoracionComponent implements OnInit {
   tituloModal = ''
   usuarioActual!: Usuario
+  descripcion!: string
+  rating: number = 0;
+  stars: number[] = [1, 2, 3, 4, 5];  // Total de estrellas
+
 
   @Input() isModalValoracionOpen: boolean = false
   @Output() close = new EventEmitter<void>()
@@ -39,6 +46,10 @@ export class ModalValoracionComponent implements OnInit {
       this.usuarioActual = usuarioEnLinea
 
     }
+
+  valorar(star: number) {
+    this.rating = star;
+  }
 
   closeModal() {
     this.close.emit()
