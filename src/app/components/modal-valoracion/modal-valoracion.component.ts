08b9b5c08497a8fc95c6ba7e3c9ad29d5d8bab8a
Emiliano <eadecuzzi@estudiantes.unsam.edu.ar>
2024-10-20 03:19:36 -0300

    Se puede valorar al back

--- src/app/components/modal-valoracion/modal-valoracion.component.ts
+++ src/app/components/modal-valoracion/modal-valoracion.component.ts
@@ -4,6 +4,8 @@ import { Usuario } from '../../domain/usuario'
 import { UsuariosService } from '../../services/service_usuarios/usuarios.service'
 import { UserSessionStorageService } from '../../services/service_user_session_storage/user-session-storage.service'
 import {FormsModule} from "@angular/forms";
+import {RecomendacionesService} from "../../services/service_recomendaciones/recomendaciones.service";
+import {ValoracionDTO} from "../../domain/valoracion";
 
 @Component({
   selector: 'readapp-moda-valoracion',
@@ -23,22 +25,27 @@ export class ModalValoracionComponent implements OnInit {
   tituloModal = ''
   usuarioActual!: Usuario
   descripcion!: string
+  userIdSS !: number | null
   rating: number = 0;
-  stars: number[] = [1, 2, 3, 4, 5];  // Total de estrellas
-
+  stars: number[] = [1, 2, 3, 4, 5]
+  error: string = ''
+  puedeValorar !: boolean
 
   @Input() isModalValoracionOpen: boolean = false
+  @Input() recomendacionId !: number
   @Output() close = new EventEmitter<void>()
 
   constructor(
     private userServiceUS: UsuariosService,
-    private sessionStorage: UserSessionStorageService
+    private sessionStorage: UserSessionStorageService,
+    private serviceRecomendacion : RecomendacionesService
   ) {}
 
-  async ngOnInit(): Promise<void> {
-    const userIdSS = this.sessionStorage.obtenerIDuserSS()
-    this.obtenerDatosUsuario(userIdSS)
+  ngOnInit(){
+    this.userIdSS = this.sessionStorage.obtenerIDuserSS()
+    this.obtenerDatosUsuario(this.userIdSS)
     this.tituloModal = 'Agregar Valoración'
+    this.puedeValorarLlamadaService()
   }
 
     async obtenerDatosUsuario(userIdSS : number | null ): Promise<void>{
@@ -51,11 +58,26 @@ export class ModalValoracionComponent implements OnInit {
     this.rating = star;
   }
 
+  async puedeValorarLlamadaService(){
+    if(this.userIdSS != null) {
+      const puedeValorarSR = await this.serviceRecomendacion.puedeValorarRecomendacion(this.recomendacionId, this.userIdSS)
+      this.puedeValorar = await puedeValorarSR
+    }
+  }
+
+
   closeModal() {
     this.close.emit()
   }
 
-  saveChanges() {
+  async saveChanges() {
+    if (this.rating === 0 || !this.descripcion.trim()) {
+      this.error = 'Debe completar todos los campos'
+      return
+    }
+    this.error = ''
+    const valoracionAEnviar = new ValoracionDTO(this.userIdSS as number, this.rating, this.descripcion)
+    await this.serviceRecomendacion.valorarRecomendacion(this.recomendacionId, valoracionAEnviar)
     this.closeModal()
   }
 
