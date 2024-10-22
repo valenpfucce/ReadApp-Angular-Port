76294ed97f0d232eaa36d0e1fa52fe9a125687b5
TatiSabba <tatisabba@gmail.com>
2024-10-22 10:40:11 -0300

    Test barra, arreglo post a get

--- src/app/components/barra-busqueda/barra-busqueda.component.spec.ts
+++ src/app/components/barra-busqueda/barra-busqueda.component.spec.ts
@@ -1,23 +1,60 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing'
+import {getHttpClientSpy} from '../../services/service_usuarios/httpClientSpy'
+import {HttpClient} from '@angular/common/http'
+import {ActivatedRoute, Router} from '@angular/router'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {BarraBusquedaComponent} from "./barra-busqueda.component";
+import {By} from "@angular/platform-browser";
 
-// import { BarraBusquedaComponent } from './barra-busqueda.component';
+describe('BarraBusquedaComponent', () => {
+  let component: BarraBusquedaComponent
+  let fixture: ComponentFixture<BarraBusquedaComponent>
+  let routerSpy: jasmine.SpyObj<Router>
+  let routeSpy: jasmine.SpyObj<ActivatedRoute>
+  let httpClientSpy: jasmine.SpyObj<HttpClient>
 
-// describe('BarraBusquedaComponent', () => {
-//   let component: BarraBusquedaComponent;
-//   let fixture: ComponentFixture<BarraBusquedaComponent>;
+  beforeEach(fakeAsync(() => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate'])
+    httpClientSpy = getHttpClientSpy()
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [BarraBusquedaComponent]
-//     })
-//     .compileComponents();
+    TestBed.configureTestingModule({
+      imports: [BarraBusquedaComponent, ReactiveFormsModule, FormsModule],
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy },
+        { provide: Router, useValue: routerSpy },
+      ]
+    }).compileComponents()
 
-//     fixture = TestBed.createComponent(BarraBusquedaComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(BarraBusquedaComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    fixture.whenStable()
+    fixture.detectChanges()
+  }))
+
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('debería hacer correctamente el binding de palabraABuscar', fakeAsync(() => {
+    const inputElement = fixture.debugElement.nativeElement.querySelector('input[data-testid="texto"]')
+
+    inputElement.value = 'Libro'
+    inputElement.dispatchEvent(new Event('input'))
+
+    fixture.detectChanges()
+
+    expect(component.palabraABuscar).toBe('Libro')
+  }))
+
+  // it('debería mostrar el checkbox en la ruta "mis-recomendaciones"', () => {
+  //   component.ngOnInit()
+  //   fixture.detectChanges()
+  //   //expect(component.showCheckBox).toBeTrue();
+  //
+  //   const checkboxElement = fixture.debugElement.nativeElement.querySelector('input[data-testid="checkbox"]')
+  //
+  //   expect(checkboxElement).toBeTruthy()
+  // })
+})
